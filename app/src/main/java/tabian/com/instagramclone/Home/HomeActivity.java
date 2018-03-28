25e85a4e3fda3715be483f9199c5db3f70ab864d
Alex Yu <j620656786206@gmail.com>
2018-03-28 18:13:56 -0500

    delete the original files and copy the files from the author

---
+++ app/src/main/java/tabian/com/instagramclone/Home/HomeActivity.java
@@ -0,0 +1,66 @@
+package tabian.com.instagramclone.Home;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import tabian.com.instagramclone.R;
+import tabian.com.instagramclone.Utils.BottomNavigationViewHelper;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+    private static final int ACTIVITY_NUM = 0;
+
+    private Context mContext = HomeActivity.this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
