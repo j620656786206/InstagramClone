bda237238754b7fc347b0eb964c40b43d5ceee69
Alex Yu <j620656786206@gmail.com>
2018-03-29 18:14:50 -0500

    1 - Building the User Profile (Part 8)

--- app/src/main/java/tabian/com/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/tabian/com/instagramclone/Profile/ProfileActivity.java
@@ -30,7 +30,7 @@ public class ProfileActivity extends AppCompatActivity{
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
 
-        //setupBottomNavigationView();
+        setupBottomNavigationView();
         setupToolBar();
     }
 
