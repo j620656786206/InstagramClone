123784d92b2d36fbb04a449c934635107c6760bf
Alex Yu <j620656786206@gmail.com>
2018-04-10 13:25:25 -0500

    Get started with Firebase (Part 19)

---
+++ app/src/main/java/tabian/com/instagramclone/Login/RegisterActivity.java
@@ -0,0 +1,26 @@
+package tabian.com.instagramclone.Login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import tabian.com.instagramclone.R;
+
+/**
+ * Created by j on 2018/4/10.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Log.d(TAG, "onCreate: started.");
+
+
+    }
+}
